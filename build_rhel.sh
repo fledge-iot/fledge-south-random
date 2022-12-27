a17062fc1a44bc53077dbc8e4a09458f3fe3d586
ashish-jabble <ashish@dianomic.com>
2022-12-27 11:58:00 +0530

    compilation fixes on CentOS9 Stream
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- build_rhel.sh
+++ build_rhel.sh
@@ -3,10 +3,14 @@
 # Pass any cmake options this way:
 
 # ./build_rhel.sh -DFLEDGE_INSTALL=/some_path/Fledge
-
-source scl_source enable devtoolset-7
-export CC=$(scl enable devtoolset-7 "command -v gcc")
-export CXX=$(scl enable devtoolset-7 "command -v g++")
+os_version=$(grep -o '^VERSION_ID=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')
+# Use scl_source only if OS is RedHat/CentOS 7
+if [[ $os_version == *"7"* ]]
+then
+    source scl_source enable devtoolset-7
+    export CC=$(scl enable devtoolset-7 "command -v gcc")
+    export CXX=$(scl enable devtoolset-7 "command -v g++")
+fi
 
 mkdir -p build
 cd build/
